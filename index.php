3074d4798556b029cf9f4682dd2b4d4d49c1ec68
Blake-Faythe <36810781+Blake-Faythe@users.noreply.github.com>
2018-03-07 13:59:27 +0100

    Création index.php
    Base Célia

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Koala Tea-Time </title>
+    <link href="style.css" media="all" rel="stylesheet" type="text/css" />
+
+		<meta charset="utf8"/>
+
+	</head>
+	<body>
+
+    <div class="header">
+      <div class="logo">  <img src="/projet/images/koala.jpg" align="absmiddle" height="95" width="80" /> </div>
+      <div class="marque">   Koala Tea-Time       </div>
+    </div>
+
+    <div class="nav">
+
+    </div>
+	</body>
+</html>
+
