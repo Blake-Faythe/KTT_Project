b0228eff23bbf80c2a3de601d9079db5f212fce9
Blake-Faythe <36810781+Blake-Faythe@users.noreply.github.com>
2018-03-09 22:29:36 +0100

    Création page des thés
    Présente deux manières de présenter : Boîtes CSS ou tableaux HTML
    
    TODO : Gros problème sur le display des produits : A voir avec la stylesheet
    Problème de retours à la ligne

---
+++ produits_the.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Une structure de base pour le site, à modifier ultérieurement -->
+<div class="degrade">
+	<head>
+       <link href="style.css" media="all" rel="stylesheet" type="text/css" />
+	   <meta charset="utf8"/>
+		<title>Les thés de Koala-Tea Time</title>	
+		
+		<?php include('header.php'); ?>
+		
+ 			<br/><br/>
+	</head>
+ 
+
+ 
+ 	<body>
+
+	<h1>Nos thés qui vont vous réchauffer les fesses ♥</h1>
+		<br/>
+		<br/>
+		<div class="boite_pdt"> 
+			<div class="img_produit"><img src="Images\green_tea.jpg" height="250"/></div>
+			<br/>
+			<div class="description">Le fabuleux thé vert de Chine</div>
+		</div>
+		
+		<div class="boite_pdt"> 
+			<div class="img_produit"><img src="Images\black_tea.jpg" height="250"/></div>
+			<br />
+			<div class="description">Le thé corsé des hommes poilus</div>
+			<br />
+			</div>
+	
+	<br />
+	
+		<div class="boite_pdt"> 
+			<div class="img_produit"><img src="Images\oolong_tea.jpg" height="250"/></div>
+			<br />
+			<div class="description">"Omae wa mou... Shindeiru!" *BJIIIIIING* (Oolong tea)</div>
+		</div>
+	
+		<div class="boite_pdt"> 
+			<div class="img_produit"><img src="Images\green_tea.jpg" height="250"/></div>
+			<br />
+			<div class="description">Le fabuleux thé vert de Chine</div>
+		</div>
+<p>
+	<br/><br/><br/><br />
+</p>
+		<center><table class="tableau_produits">
+				<!--<thead> -->
+					<tr>
+						<td><div class="img_produit"><img src="Images\green_tea.jpg" height="250"/></div>
+			
+			<br/>
+			<description>Le fabuleux thé vert de Chine</description> </td>
+			
+						<td> <div class="img_produit"><img src="Images\black_tea.jpg" height="250"/></div>
+			
+			<br/>
+			<description>Le thé corsé des hommes poilus</description> </td>
+			<td><br/><br/></td>
+				 	</tr>
+		
+					<tr>
+						<td> <div class="img_produit"><img src="Images\oolong_tea.jpg" height="250"/></div>
+			
+			<br/>
+			<description>"Omae wa mou... Shindeiru!" *BJIIIIIING* (Oolong tea)</description> </td>
+			
+						<td> <div class="img_produit"><img src="Images\black_tea.jpg" height="250"/></div>
+			
+			<br/>
+			<description>Le thé corsé des hommes poilus</description> </td>
+					</tr>
+				<!-- </tbody> -->
+
+
+			</table></center>
+			
+			
+			
+				<br/>
+			
+		<h2>2ème section</h2>
+			<p>Paragraphe</p>
+		
+				<br/>
+			
+		<h3>3ème section</h3>
+			<p>Paragraphe</p>
+		
+	</body>
+	</div>	
+</html>
+		
