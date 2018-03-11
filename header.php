76d7d1cc7f3e4b17be61b6a306121f117b7e40b2
Blake-Faythe <36810781+Blake-Faythe@users.noreply.github.com>
2018-03-11 15:08:36 +0100

    Le header
    Ajout d'un header php à menu non-déroulants.

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Koala Tea-Time </title>
+    <link href="style.css" media="all" rel="stylesheet" type="text/css" />
+
+		<meta charset="utf8"/>
+
+	</head>
+	<body>
+	
+	<!-- Logo et barres de couleur tout en haut -->
+    
+	<div class="header">
+      <div class="logo">  <img src="Images/koala.jpg" align="absmiddle" height="95" width="80" /> </div>
+      <div class="marque">   Koala Tea-Time       </div>
+    </div>
+	<div class="page">
+    <div class="nav">
+	</div>
+	<br/>
+	<!-- Menu en liste horizontale -->
+		<div id="menulist">
+				<ul id="menu">
+					<li> <a href="urlbidon.com"> Accueil </a> </li>
+					
+					<li> <a href="urlbidon.com">Nos produits </a> </li>
+				<!--	<ul>
+							<li> <a href="urlbidon.com">Nos thés </a> </li>
+							<li> <a href="urlbidon.com">Nos cafés </a> </li>
+							<li> <a href="urlbidon.com">Nos gourmandises </a> </li>
+						</ul>	
+				-->
+					<li> <a href="urlbidon.com">Qui sommes-nous? </a> </li>
+					
+					<li><a href="urlbidon.com">Mon compte </a> </li>
+				<!--	<ul>
+							<li> <a href="urlbidon.com">Mes informations </a> </li>
+							<li> <a href="urlbidon.com">Se déconnecter </a> </li>
+						</ul>	
+				-->
+					<li><a href="urlbidon.com">Zbay </a> </li>
+				</ul>
+		</div>		
+		
+			
+	</div>		
+	</body>
+</html>
