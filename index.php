fce28f2e6b7f87295f99507d99036f0541162a2c
Blake-Faythe <36810781+Blake-Faythe@users.noreply.github.com>
2018-03-14 10:10:34 +0100

    Index à image
    Update de l'index : Inclusion du header normalisé
    Ajout d'un footer
    
    Image centrale d'accueil avec trois lien intégrés (le lien d'accueil et de la page des thés est FONCTIONNEL)

--- index.php
+++ index.php
@@ -1,22 +1,30 @@
 <!DOCTYPE html>
 <html>
+
+<div class="degrade">
 	<head>
-		<title> Koala Tea-Time </title>
-    <link href="style.css" media="all" rel="stylesheet" type="text/css" />
-
+       <link href="style.css" media="all" rel="stylesheet" type="text/css" />
 		<meta charset="utf8"/>
-
+		<title>Koala-Tea Time</title>	
+		
+		<?php include('header.php'); ?>
+	
 	</head>
+	
 	<body>
 
-    <div class="header">
-      <div class="logo">  <img src="/projet/images/koala.jpg" align="absmiddle" height="95" width="80" /> </div>
-      <div class="marque">   Koala Tea-Time       </div>
-    </div>
+			<div class="imgindex"> <img src="Images/tea_crops.jpg" width="100%" height="auto"/>
+				<div class="bandeauindex"> FEEL THE QUALITY!
+					<div class="produitindex1"> <a href="produits_the.php">Nos thés</a></div>
+					<div class="produitindex2"> <a href="urlbidon.com">Nos cafés</a></div>
+					<div class="produitindex3"> <a href="urlbidon.com">Nos gourmandises</a></div>
+				</div>
+				
+			</div>
 
-    <div class="nav">
-
-    </div>
+		<div class="footer"> (c) Koala-Tea Time 2018 </div>
+		</div>
 	</body>
+	
+	
 </html>
-
