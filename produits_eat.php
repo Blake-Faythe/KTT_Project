574d8250c9278f3a3493cbdf2558ad0b1c247c2b
Blake-Faythe <36810781+Blake-Faythe@users.noreply.github.com>
2018-03-15 13:39:56 +0100

    Add produits_eat
    Page des produits "Gourmandises"

---
+++ produits_eat.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+	<div class="degrade">
+		<head>
+			<link href="style.css" media="all" rel="stylesheet" type="text/css" />
+			<meta charset="utf8"/>
+			<title>Les gourmandises de Koala-Tea Time</title>	
+		
+			<?php include('header.php'); ?>
+		
+			</head>
+ 
+		<body>
+			<div class="imgtitre"> <img src="Images/bandeau_titre.jpg" width="100%" height="auto"/> 
+				<h1>Nos gourmandises succulentes ♥</h1>
+			</div>
+
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+		
+				<?php include('footer.php'); ?>
+		
+		</body>
+		
+	</div>	
+	
+</html>
