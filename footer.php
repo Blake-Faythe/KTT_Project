e2085581c0ea82a1124ca3b20ce62f67c950154a
Blake-Faythe <36810781+Blake-Faythe@users.noreply.github.com>
2018-03-15 10:45:10 +0100

    Add footer
    Ajout d'un petit footer à inclure en bas de chaque page

---
+++ footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Koala Tea-Time </title>
+    <link href="style.css" media="all" rel="stylesheet" type="text/css" />
+
+		<meta charset="utf8"/>
+
+	</head>
+	<body>
+	
+	<!-- Footer tout en bas -->
+
+		<div class="footer"> (c) Koala-Tea Time 2018 </div>
+
+	</body>
+</html>
