a10a8cb63c6e223a4aa7b860e47e6734a693ef49
Blake-Faythe <36810781+Blake-Faythe@users.noreply.github.com>
2018-03-15 13:33:04 +0100

    Grosse update de la page authentification
    Authentification devient l'espace pour enter un login et mdp.
    Renvoie sur la page welcome_login si authentification réussie.

--- authentification.php
+++ authentification.php
@@ -1,44 +1,36 @@
 <!DOCTYPE html>
 
-<?php session_start(); ?>
 <html>
-
-<!-- Une structure de base pour le site, à modifier ultérieurement -->
-<div class="degrade">
-	<head>
-       <link href="style.css" media="all" rel="stylesheet" type="text/css" />
-	   <meta charset="utf8"/>
-		<title>Les thés de Koala-Tea Time</title>	
+	<div class="degrade">
+		<head>
+			<link href="style.css" media="all" rel="stylesheet" type="text/css" />
+			<meta charset="utf8"/>
+			<title>S'authentifier sur Koala-Tea Time</title>	
 		
-		<?php include('header.php'); ?>
+			<?php include('header.php'); ?>
 		
- 			<br/><br/>
-	</head>
- 
+		</head>
 
- 
  	<body>
-
-	<h1>Authentification</h1>
+		<div class="imgtitre"> <img src="Images/bandeau_titre.jpg" width="100%" height="auto"/> 
+			<h1>S'authentifier</h1>
+		</div>
 	
-		<?php
-			$login = $_POST ["login"];
-			$mdp = $_POST ["mdp"];
-			
-		 	if($login == "admin" AND $mdp== "admin") {
-				$_SESSION["login"]=$login;
-					
-				echo "<p> Bienvenue, $login </p>";
-			}
-			
-			else {
-				echo "<p> Authentification refusée </p>";
-			}
-			
-		?>
+		<form name="Idenfication" action="welcome_login.php" method="post">
+		<br/>
 		
-		<a href="account.php"> Mon compte </a>
-		<a href="deco.php"> Déconnexion </a>
+			<label> Login : </label>
+			<input type="text" name="login" maxlength="15" />
+			<br/>
+			
+			<label> Mot de passe : </label>
+			<input type="text" name="mdp" maxlength="15" />
+			<input type="submit" value="S'authentifier" />
+			
+		</form>
+		<br/>
+		
+	<?php include('footer.php'); ?>
 	
 	</body>
 	
