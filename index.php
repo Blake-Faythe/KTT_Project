2b7a0aeea82869c60e4e02b2dd9140e88e48b99b
Blake-Faythe <36810781+Blake-Faythe@users.noreply.github.com>
2018-03-15 16:09:44 +0100

    Update de l'index
    Maintenant avec des URL fonctionnelles

--- index.php
+++ index.php
@@ -1,30 +1,28 @@
 <!DOCTYPE html>
 <html>
 
-<div class="degrade">
-	<head>
-       <link href="style.css" media="all" rel="stylesheet" type="text/css" />
-		<meta charset="utf8"/>
-		<title>Koala-Tea Time</title>	
+	<div class="degrade">
+		<head>
+			<link href="style.css" media="all" rel="stylesheet" type="text/css" />
+			<meta charset="utf8"/>
+			<title>Koala-Tea Time</title>	
 		
 		<?php include('header.php'); ?>
+		</head>
 	
-	</head>
-	
-	<body>
-
+		<body>
 			<div class="imgindex"> <img src="Images/tea_crops.jpg" width="100%" height="auto"/>
 				<div class="bandeauindex"> FEEL THE QUALITY!
 					<div class="produitindex1"> <a href="produits_the.php">Nos thés</a></div>
-					<div class="produitindex2"> <a href="urlbidon.com">Nos cafés</a></div>
-					<div class="produitindex3"> <a href="urlbidon.com">Nos gourmandises</a></div>
-				</div>
-				
+					<div class="produitindex2"> <a href="produits_cafe.php">Nos cafés</a></div>
+					<div class="produitindex3"> <a href="produits_eat.php">Nos gourmandises</a></div>
+				</div>	
 			</div>
-
-		<div class="footer"> (c) Koala-Tea Time 2018 </div>
-		</div>
-	</body>
-	
+		
+			<?php include('footer.php'); ?>
+			
+		</body>
+		
+	</div>
 	
 </html>
