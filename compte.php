7b347afd6971abf6a879ec3b0cbafd9b32f9c972
Blake-Faythe <36810781+Blake-Faythe@users.noreply.github.com>
2018-03-15 13:37:43 +0100

    Add compte
    Page proposant les options de connexion, déconnexion.
    TODO : Modification des infos, toussa

---
+++ compte.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+	<div class="degrade">
+		<head>
+			<link href="style.css" media="all" rel="stylesheet" type="text/css" />
+			<meta charset="utf8"/>
+			<title>Mon compte sur Koala-Tea Time</title>	
+		
+		<?php include('header.php'); ?>
+		
+		</head>
+ 
+		<body>
+			<div class="imgtitre"> <img src="Images/bandeau_titre.jpg" width="100%" height="auto"/> 
+				<h1>Mon compte</h1>
+			</div>
+			<br/>
+			<br/>
+		
+			<a href="authentification.php"> S'authentifier </a>
+			<br/>
+			<a href="deco.php"> Se déconnecter </a>
+
+			<br/>
+			<br/>
+		
+			<?php include('footer.php'); ?>
+		
+		</body>
+		
+	</div>	
+	
+</html>
