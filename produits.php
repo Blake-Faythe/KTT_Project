ed9388ba9bdc396e624af64d22bc590435ae285c
Blake-Faythe <36810781+Blake-Faythe@users.noreply.github.com>
2018-03-15 13:30:28 +0100

    Add produits
    Page produits à recompléter ultérieurement, donnant un aperçu des produits et le choix entre trois catégories

---
+++ produits.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+	<div class="degrade">
+		<head>
+			<link href="style.css" media="all" rel="stylesheet" type="text/css" />
+			<meta charset="utf8"/>
+			<title>Les produits de Koala-Tea Time</title>	
+		
+			<?php include('header.php'); ?>
+		
+		</head>
+ 
+
+ 
+		<body>
+
+			<div class="imgtitre"> <img src="Images/bandeau_titre.jpg" width="100%" height="auto"/> 
+				<h1>Nos produits</h1>
+			</div>
+
+				<br/>
+				<br/>
+			
+				<a href="produits_the.php">Nos thés</a>
+				<br/>
+			
+				<a href="produits_cafe.php">Nos cafés</a>
+				<br/>
+			
+				<a href="produits_eat.php">Nos gourmandises</a>
+
+				<br/>
+				<br/>
+		
+					<?php include('footer.php'); ?>
+		
+		</body>
+	</div>	
+</html>
+		
