7465666ba4c4cdf38a72144e24e92165de39de16
Blake-Faythe <36810781+Blake-Faythe@users.noreply.github.com>
2018-03-15 13:38:48 +0100

    Add info
    Page résumant les informations sur Koala-Tea Time.
    
    TODO : Rédiger un laïus sur l'entreprise

---
+++ Info.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+	<div class="degrade">
+		<head>
+			<link href="style.css" media="all" rel="stylesheet" type="text/css" />
+			<meta charset="utf8"/>
+			<title>Informations sur Koala-Tea Time</title>	
+		
+			<?php include('header.php'); ?>
+		
+		</head>
+ 
+		<body>
+			<div class="imgtitre"> <img src="Images/bandeau_titre.jpg" width="100%" height="auto"/> 
+				<h1>Qui sommes-nous?</h1>
+			</div>
+
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+		
+				<?php include('footer.php'); ?>
+		
+		</body>
+		
+	</div>	
+	
+</html>
