c5be4e0e19b620012b06712e6fca9f8f0f71ba88
Blake-Faythe <36810781+Blake-Faythe@users.noreply.github.com>
2018-03-16 11:16:10 +0100

    Update page des thés
    Revue complète de la page des thés avec un display produits responsive

--- produits_the.php
+++ produits_the.php
@@ -5,92 +5,52 @@
 <div class="degrade">
 	<head>
        <link href="style.css" media="all" rel="stylesheet" type="text/css" />
-	   <meta charset="utf8"/>
+		<meta charset="utf8"/>
 		<title>Les thés de Koala-Tea Time</title>	
 		
 		<?php include('header.php'); ?>
 		
- 			<br/><br/>
 	</head>
  
 
  
  	<body>
 
-	<h1>Nos thés qui vont vous réchauffer les fesses ♥</h1>
-		<br/>
-		<br/>
-		<div class="boite_pdt"> 
-			<div class="img_produit"><img src="Images\green_tea.jpg" height="250"/></div>
-			<br/>
-			<div class="description">Le fabuleux thé vert de Chine</div>
+		<div class="imgtitre"> <img src="Images/bandeau_titre.jpg" width="100%" height="auto"/> 
+		<h1>Nos thés de qualité ♥</h1>
 		</div>
-		
-		<div class="boite_pdt"> 
-			<div class="img_produit"><img src="Images\black_tea.jpg" height="250"/></div>
-			<br />
-			<div class="description">Le thé corsé des hommes poilus</div>
-			<br />
+		<div class="backproducts"> 
+				<div class="productbox"> 
+					<div class="imgproduct"> <img src="Images\green_tea.jpg" width="100%"/>
+					</div>
+					<div class="titreproduct">Produit 1</div>
+					<div class="descriptionproduct"> Description du produit 1 </div>
+				</div>
+				
+				<div class="productbox"> 
+					<div class="imgproduct"> <img src="Images\green_tea.jpg" width="100%"/>
+					</div>
+					<div class="titreproduct">Produit 2</div>
+					<div class="descriptionproduct"> Description du produit 2 </div>
+				</div>
+				
+				<div class="productbox"> 
+					<div class="imgproduct"> <img src="Images\green_tea.jpg" width="100%"/>
+					</div>
+					<div class="titreproduct">Produit 3</div>
+					<div class="descriptionproduct"> Description du produit 3 </div>
+				</div>
+				
+				<div class="productbox"> 
+					<div class="imgproduct"> <img src="Images\green_tea.jpg" width="100%"/>
+					</div>
+					<div class="titreproduct">Produit 4</div>
+					<div class="descriptionproduct"> Description du produit 4 </div>
+				</div>
+				
 			</div>
-	
-	<br />
-	
-		<div class="boite_pdt"> 
-			<div class="img_produit"><img src="Images\oolong_tea.jpg" height="250"/></div>
-			<br />
-			<div class="description">"Omae wa mou... Shindeiru!" *BJIIIIIING* (Oolong tea)</div>
-		</div>
-	
-		<div class="boite_pdt"> 
-			<div class="img_produit"><img src="Images\green_tea.jpg" height="250"/></div>
-			<br />
-			<div class="description">Le fabuleux thé vert de Chine</div>
-		</div>
-<p>
-	<br/><br/><br/><br />
-</p>
-		<center><table class="tableau_produits">
-				<!--<thead> -->
-					<tr>
-						<td><div class="img_produit"><img src="Images\green_tea.jpg" height="250"/></div>
-			
-			<br/>
-			<description>Le fabuleux thé vert de Chine</description> </td>
-			
-						<td> <div class="img_produit"><img src="Images\black_tea.jpg" height="250"/></div>
-			
-			<br/>
-			<description>Le thé corsé des hommes poilus</description> </td>
-			<td><br/><br/></td>
-				 	</tr>
 		
-					<tr>
-						<td> <div class="img_produit"><img src="Images\oolong_tea.jpg" height="250"/></div>
-			
-			<br/>
-			<description>"Omae wa mou... Shindeiru!" *BJIIIIIING* (Oolong tea)</description> </td>
-			
-						<td> <div class="img_produit"><img src="Images\black_tea.jpg" height="250"/></div>
-			
-			<br/>
-			<description>Le thé corsé des hommes poilus</description> </td>
-					</tr>
-				<!-- </tbody> -->
-
-
-			</table></center>
-			
-			
-			
-				<br/>
-			
-		<h2>2ème section</h2>
-			<p>Paragraphe</p>
-		
-				<br/>
-			
-		<h3>3ème section</h3>
-			<p>Paragraphe</p>
+				<?php include('footer.php'); ?>
 		
 	</body>
 	</div>	
